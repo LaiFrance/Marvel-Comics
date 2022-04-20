f4a10e11b9ac8a0d644c33ad79123eef26f9ba19
Laise France <laise94@hotmail.com>
2022-04-20 18:16:58 -0300

    add navbar

--- my-Marvel-app/src/app/app.module.ts
+++ my-Marvel-app/src/app/app.module.ts
@@ -11,6 +11,13 @@ import { PesquisarComponent } from './pesquisar/pesquisar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SettingsComponent } from './settings/settings.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -20,12 +27,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PersonagensComponent,
     ComicsComponent,
     PesquisarComponent,
-    SettingsComponent
+    SettingsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
